dc284b9c59c17bf727576975c4b5aaad7d6b6b93
taylorzhong <taylorzhong@tencent.com>
2023-08-05 21:58:56 +0800

    gaijiegou

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include "includes/Gun.h"
 #include "includes/Soldier.h"
 
-void test() {
+void SoliderShoot() {
     Solider sanduo("xusanduo");
     sanduo.AddGun(new Gun("AK47"));
     sanduo.AddBulletToGun(20);
     sanduo.fire();
 }
 int main() {
-    test();
+    SoliderShoot();
     return 0;
 }

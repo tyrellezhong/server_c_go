aa7894b2a6971dae52469c79685592ed930d227d
taylorzhong <taylorzhong@tencent.com>
2024-03-08 15:00:51 +0800

    protobuf test

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 #include "Sockets.h"
+#include "src/proto_test.pb.h"
 
 
 
@@ -34,15 +35,23 @@ struct make_index_seq<0, M...> : public index_seq<M...> {};
 
 
 template <int... N> void print(index_seq<N...>) {
-  (void)std::initializer_list<int>{((std::cout << N << " "), 0)...};
+  // (void)std::initializer_list<int>{((std::cout << N << " "), 0)...};
+  ((std::cout << N << " "), ...);
+  std::cout << std::endl;
 }
 
 template <size_t... N> void print(std::index_sequence<N...>) {
   std::vector<int> res;
-  (void)std::initializer_list<int>{
-      ((res.push_back(N), std::cout << N << " "), 0)...};
-    std::cout << std::endl;
+  auto xx = std::initializer_list<int>{((res.push_back(N), std::cout << N << " ", 10))...};
+  std::cout << "list size:" << xx.size() << std::endl;
+  for (auto iter = xx.begin(); iter != xx.end(); ++iter)
+  {
+    std::cout << *iter << " ";
+  }
+  std::cout << std::endl;
+  std::cout << "For each : ";
   std::for_each(res.begin(), res.end(), [](int x) {std::cout << x << " ";});
+  std::cout << std::endl;
 }
 
 void SoliderShoot() {
@@ -56,10 +65,11 @@ int main() {
     // int a = 10;
     // int b = 5;
     // std::cout << "a > b : " << Greater(a, b) << std::endl;
-    // auto t = std::make_index_sequence<10>();
+    auto t = std::make_index_sequence<10>();
     // print(t);
-    // auto t2 = make_index_seq<5>();
-    // print(t2);
+    auto t2 = make_index_seq<5>();
+    print(t2);
+;
     // std::vector<int32_t> Numbers{0,1,2,3,4,5};
     // std::string string_test("helloworld");
     // // static_assert(const_nums<101>(100) == 100 * 100);
@@ -69,7 +79,9 @@ int main() {
     // TCPServer();
     // UDPServer();
     // TCPClient();
-     UDPClient();
+    //  UDPClient();
+    tutorial::Person Person;
+    auto Descripter = Person.GetDescriptor();
     return 0;
 
 }

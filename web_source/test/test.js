105a4d7909b0c87fded251e5e1e448a54df73ec5
taylorzhong <taylorzhong@tencent.com>
2025-04-10 16:21:58 +0800

    feature : 增加html测试文件

--- web_source/test/test.js
+++ web_source/test/test.js
@@ -1,1 +1,14 @@
 alert("C语言中文网：c.biancheng.net");
+
+var res = window.confirm("这里是要显示的内容");
+if(res == true){
+    alert("你点击了“确定”按钮");
+}else{
+    alert("你点击了“取消”按钮");
+}
+
+var myArr = ["Chrome","Firefox","Edge","Safari","Opera"];
+console.log(myArr);
+
+document.write("<p>现在的时间是：</p>");
+document.write(Date());

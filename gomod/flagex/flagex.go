51908fd28b764ac44d142e6b58d098d2bc120ce2
taylorzhong <taylorzhong@.tencent.com>
2025-06-28 14:44:23 +0800

    feature : 编码

--- gomod/flagex/flagex.go
+++ gomod/flagex/flagex.go
@@ -1,7 +1,11 @@
 package flagex
 
 import (
+	"encoding/base64"
 	"flag"
+	"fmt"
+	"log"
+	"os"
 )
 
 var (
@@ -19,4 +23,23 @@ func PrintFlagArgs() {
 	flag.Parse()
 	println("name:", *ArgName)
 	println("bool:", *ArgBool)
+
+	// 这是你提供的 Base64 编码的字符串
+	encodedStr := *ArgName
+	if encodedStr == "" {
+		encodedStr = os.Args[1]
+	}
+	println("encodedStr:", encodedStr)
+
+	// Base64 解码
+	decodedBytes, err := base64.StdEncoding.DecodeString(encodedStr)
+	if err != nil {
+		log.Fatalf("Failed to decode Base64 string: %v", err)
+	}
+
+	// 将解码后的字节转换为 UTF-8 字符串
+	utf8String := string(decodedBytes)
+
+	// 打印还原的 UTF-8 字符串
+	fmt.Println("Decoded UTF-8 string:", utf8String)
 }
--- gomod/main.go
+++ gomod/main.go
@@ -6,7 +6,7 @@ import (
 	"runtime"
 
 	// _ "net/http/pprof" // 确保这里是匿名导入
-	txttemplate "gomod/txt_template"
+	"gomod/flagex"
 )
 
 func main() {
@@ -16,7 +16,7 @@ func main() {
 	// 设置日志格式，包含文件名和行号
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	txttemplate.ConditionTest()
+	// txttemplate.ConditionTest()
 
 	// httpex.PprofServer()
 	// httpex.GinServer()
@@ -30,4 +30,5 @@ func main() {
 	// interfaceex.InterfaceTest()
 	// zlog.NewCustomZLog()
 	// httpex.UrlEncode()
+	flagex.PrintFlagArgs()
 }
